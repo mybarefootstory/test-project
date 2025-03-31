ec756afee69af19cf43ffb8b84d66e9146d3bd2d
mybarefootstory <akashkumarswarnkar7172@gmail.com>
2025-03-31 20:48:01 +0530

    notebooks created

--- src/logger.py
+++ src/logger.py
@@ -15,5 +15,5 @@ logging.basicConfig(
     filemode='w'
 )
 
-if __name__ == "__main__":
-    logging.info("Logging has started")
+# if __name__ == "__main__":
+#     logging.info("Logging has started")
